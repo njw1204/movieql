5408d37b68c19b8643f6e5463263d4c2212878cf
Na Jongwoo <njw1204@naver.com>
2022-06-28 22:31:08 +0900

    GraphQL Schema Definition Language

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,51 @@
 import {ApolloServer, gql} from 'apollo-server';
 
-const server = new ApolloServer({});
+const typeDefs = gql`
+  type Query {
+    movies(minimumRating: Float): [Movie!]!
+    movie(id: ID!): Movie
+    comments: [Comment!]!
+    comment(id: ID!): Comment
+  }
+
+  type Mutation {
+    postComment(movieId: ID!, message: String!): Comment
+    deleteComment(id: ID!): Boolean!
+  }
+
+  type Movie {
+    id: ID!
+    url: String
+    imdb_code: String
+    title: String
+    year: Int
+    rating: Float
+    runtime: Int
+    genres: [String!]
+    summary: String
+    description_full: String
+    synopsis: String
+    background_image: String
+    small_cover_image: String
+    medium_cover_image: String
+    large_cover_image: String
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    message: String!
+  }
+`;
+
+const resolvers = {
+  Query: {},
+  Mutation: {},
+  Movie: {},
+  Comment: {},
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({url}) => {
   console.log(`Running on ${url}`);
