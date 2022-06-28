b111aa6f94dc5688d4d5c114f082efb5f808aee8
Na Jongwoo <njw1204@naver.com>
2022-06-28 23:39:52 +0900

    GraphQL Descriptions

--- src/index.ts
+++ src/index.ts
@@ -11,17 +11,46 @@ import {getMovie, getMovies} from './repositories/movie-repository';
 
 const typeDefs = gql`
   type Query {
+    """
+    영화 목록 조회
+
+    (minimumRating: 최소 평점 필터링)
+    """
     movies(minimumRating: Float): [Movie!]!
+
+    """
+    영화 단건 조회
+    """
     movie(id: ID!): Movie
+
+    """
+    댓글 목록 조회
+
+    (movieId: 해당 영화의 댓글만 필터링)
+    """
     comments(movieId: ID): [Comment!]!
+
+    """
+    댓글 단건 조회
+    """
     comment(id: ID!): Comment
   }
 
   type Mutation {
+    """
+    댓글 작성
+    """
     postComment(movieId: ID!, message: String!): Comment
+
+    """
+    댓글 삭제
+    """
     deleteComment(id: ID!): Boolean!
   }
 
+  """
+  영화
+  """
   type Movie {
     id: ID!
     url: String
@@ -38,12 +67,21 @@ const typeDefs = gql`
     small_cover_image: String
     medium_cover_image: String
     large_cover_image: String
+    """
+    댓글 목록
+    """
     comments: [Comment!]!
   }
 
+  """
+  댓글
+  """
   type Comment {
     id: ID!
     message: String!
+    """
+    댓글의 대상 영화
+    """
     fromMovie: Movie
   }
 `;
--- src/repositories/movie-repository.ts
+++ src/repositories/movie-repository.ts
@@ -4,7 +4,7 @@ import Movie from '../entities/movie';
 export function getMovies(minimumRating?: number) {
   const url = minimumRating
     ? `https://yts.mx/api/v2/list_movies.json?minimum_rating=${minimumRating}&sort_by=year`
-    : `https://yts.mx/api/v2/list_movies.json?sort_by=year`;
+    : 'https://yts.mx/api/v2/list_movies.json?sort_by=year';
 
   return fetch(url)
     .then(
