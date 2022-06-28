41086a15cd02d6aa93c3ba8e6b5a2b30fabab062
Na Jongwoo <njw1204@naver.com>
2022-06-28 23:28:56 +0900

    GraphQL Resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import {ApolloServer, gql} from 'apollo-server';
+import Comment from './entities/comment';
+import Movie from './entities/movie';
+import {
+  deleteComment,
+  getComment,
+  getComments,
+  postComment,
+} from './repositories/comment-repository';
+import {getMovie, getMovies} from './repositories/movie-repository';
 
 const typeDefs = gql`
   type Query {
     movies(minimumRating: Float): [Movie!]!
     movie(id: ID!): Movie
-    comments: [Comment!]!
+    comments(movieId: ID): [Comment!]!
     comment(id: ID!): Comment
   }
 
@@ -35,14 +44,53 @@ const typeDefs = gql`
   type Comment {
     id: ID!
     message: String!
+    fromMovie: Movie
   }
 `;
 
 const resolvers = {
-  Query: {},
-  Mutation: {},
-  Movie: {},
-  Comment: {},
+  Query: {
+    movies(_: unknown, {minimumRating}: {minimumRating?: number}) {
+      return getMovies(minimumRating);
+    },
+
+    movie(_: unknown, {id}: {id: string}) {
+      return getMovie(Number(id));
+    },
+
+    comments(_: unknown, {movieId}: {movieId?: string}) {
+      return getComments(Number(movieId));
+    },
+
+    comment(_: unknown, {id}: {id: string}) {
+      return getComment(id);
+    },
+  },
+
+  Mutation: {
+    postComment(
+      _: unknown,
+      {movieId, message}: {movieId: string; message: string}
+    ) {
+      return postComment(Number(movieId), message);
+    },
+
+    deleteComment(_: unknown, {id}: {id: string}) {
+      return deleteComment(id);
+    },
+  },
+
+  Movie: {
+    comments(root: Movie) {
+      return getComments(root.id);
+    },
+  },
+
+  Comment: {
+    fromMovie(root: Comment) {
+      return getMovie(root.movieId);
+    },
+  },
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
